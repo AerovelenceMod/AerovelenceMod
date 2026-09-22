5bd4a6d327a110252b93fa3cfae98d34e693eea3
Arcri <47931960+Arcri@users.noreply.github.com>
2026-09-22 04:20:33 -0600

    Bolsa de Crystal Tumbler

--- Content/Items/TreasureBags/CrystalTumblerBag.cs
+++ Content/Items/TreasureBags/CrystalTumblerBag.cs
@@ -12,7 +12,7 @@ namespace AerovelenceMod.Content.Items.TreasureBags
     public class CrystalTumblerBag : TranslatableModItem
     {
         private const string Description = "Right click to open\nContains a Tumbler weapon and the Tumbling Harness";
-        public static int[] Weapons => new[] { ModContent.ItemType<Geomagnetic>(), ModContent.ItemType<FenceSitter>(), ModContent.ItemType<TumblerAccelerator>(), ModContent.ItemType<ConductorWand>(), ModContent.ItemType<DarkCrystalStaff>() };
+        public static int[] Weapons => new[] { ModContent.ItemType<Geomagnetic>(), ModContent.ItemType<FenceSitter>(), ModContent.ItemType<TumblerAccelerator>(), ModContent.ItemType<ConductorWand>(), ModContent.ItemType<DarkCrystalStaff>(), ModContent.ItemType<Staticstring>(), ModContent.ItemType<Rubbleswarm>(), ModContent.ItemType<BatteryBackpack>() };
         public override void SetStaticDefaults()
         {
             this.ModifyLocalization("Treasure Bag (Crystal Tumbler)", Description);
@@ -20,12 +20,6 @@ namespace AerovelenceMod.Content.Items.TreasureBags
             ItemID.Sets.BossBag[Type] = true;
             ItemID.Sets.PreHardmodeLikeBossBag[Type] = true;
             Item.ResearchUnlockCount = 3;
-        }
-        public override void ModifyTooltips(List<TooltipLine> tooltips)
-        {
-            tooltips.RemoveAll(t => t.Mod == "Terraria" && t.Name.StartsWith("Tooltip"));
-            tooltips.Add(new TooltipLine(Mod, "Tooltip0", Description));
-            base.ModifyTooltips(tooltips);
         }
         public override void SetDefaults()
         {
